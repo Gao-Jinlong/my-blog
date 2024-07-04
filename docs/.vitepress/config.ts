0e92f5b01224054edf90d7dadf50b03e64dc6bda
gao-jinlong <2675130594@qq.com>
2024-07-05 01:32:51 +0800

    chore: config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -78,6 +78,10 @@ export default () => {
                 text: "前端项目搭建",
                 link: `${PRACTICE_BASE_PATH}/前端项目搭建.md`,
               },
+              {
+                text: "IndexedDB",
+                link: `${PRACTICE_BASE_PATH}/IndexedDB/index.md`,
+              },
               // {
               //   text: "openlayers with OffscreenCanvas",
               //   link: `${PRACTICE_BASE_PATH}/openlayersWithOffscreenCanvas/index.md`,
